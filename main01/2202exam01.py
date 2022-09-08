aca25f89cfb7f18b49e127dd56b8943bea4638d1
yujisu01 <jssla1@naver.com>
2022-09-08 17:50:41 +0900

    220908

---
+++ main01/2202exam01.py
@@ -0,0 +1,12 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a="REMEMBER NOVEMBER"
+>>> b=a[:3]+a[12:16];
+>>> c="R AND %s" % "STR";
+>>> print(b+c)
+REMEMBER AND STR
+>>> print(c)
+R AND STR
+>>> print(b)
+REMEMBE
+>>>
